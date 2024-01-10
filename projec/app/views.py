b0c4d20839954fc6af812eb438fb93b3740e2ae2
ashleytran3 <ashlleynt@gmail.com>
2024-01-10 14:00:19 -0800

    added app.urls, template files for homepage
    yuh

--- projec/app/views.py
+++ projec/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
+def home_page_view(request):
+    return render(request, "home.html")
--- projec/projec/settings.py
+++ projec/projec/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'projec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "base.html" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
